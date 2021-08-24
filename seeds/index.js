3a26ea3d2edbe26b46e7fecf2200d1da15811db1
Denver-Meadows <meadows8200@gmail.com>
2021-08-23 20:30:47 -0400

    DB seeded

---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+// To seed the DB we want this file to be self-contained (connected to Mongoose and use the Model)
+// Reminder:  We always run this file separate, without node, whenever we want to seed our DB.
+
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers')
+
+const mongoose = require('mongoose');
+const Campground = require('../models/campground'); // import model
+mongoose.connect('mongodb://localhost:27017/camp-review', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+// Mongoose Connection error handling
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+  console.log("Database connected");
+});
+
+// Get random number from array -- Will use function below
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// Delete everything from DB (do a test prior to seeding to make sure we are connected to the DB).  Anytime you run this file, it will delete the DB and seed it with 50 random places
+const seedDB = async () => {
+  await Campground.deleteMany({});  // delete everything 
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000)
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`, // creating made up names
+    })
+    await camp.save();
+  }
+}
+
+// Close the DB connection after calling seedDB
+seedDB().then(() => {
+  mongoose.connection.close();
+});
