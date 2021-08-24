5fb041f93112b6e4f7310afd73abbb20e78d224b
Denver-Meadows <meadows8200@gmail.com>
2021-08-23 21:39:40 -0400

    create ready

--- app.js
+++ app.js
@@ -20,19 +20,37 @@ mongoose.connection.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
   res.render('home')
-})
+});
 
+// Index
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
 })
 
+// Create
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+// 2nd Part of Create (posting data from form)
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body)
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`)
+})
+
+// Show
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', { campground })
 })
+
+
 
 // For testing
 app.get('/makecampground', async (req, res) => {
