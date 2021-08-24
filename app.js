3d2b1f02021ae3a35fc2f6537752291e1635481b
Denver-Meadows <meadows8200@gmail.com>
2021-08-23 21:09:22 -0400

    show ready

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground })
+})
+
 // For testing
 app.get('/makecampground', async (req, res) => {
   const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping'})
