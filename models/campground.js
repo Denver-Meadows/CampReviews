eb804fbb55e2988912a50466a6003aa8ef7b2201
Denver-Meadows <meadows8200@gmail.com>
2021-08-31 18:49:25 -0400

    Updated model

--- models/campground.js
+++ models/campground.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;  // shortcut for mongoose.Schema
 
 const CampgroundSchema = new Schema({
-  title: String,
-  image: String,
-  price: Number,
-  description: String,
-  location: String
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema)  // export created model
