c0a8635014f346022a5da04d37ac3bfbaf0eec41
Denver-Meadows <meadows8200@gmail.com>
2021-09-02 21:51:03 -0400

    Deleting campgrounds and reviews working

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 const Schema = mongoose.Schema;  // shortcut for mongoose.Schema
 
 const CampgroundSchema = new Schema({
@@ -29,6 +30,19 @@ const CampgroundSchema = new Schema({
       ref: "Review", // The reference model
     }
   ],
+});
+
+// Mongoose middleware for deleting.  Post will run after 'findOneAndDelete' runs. Which is being ran when we delete a campground with 'findByIdAndDelete'.  Check the mongoose docs but this is their middleware that runs.
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  // console.log(doc) // the doc is the data that was deleted.
+  // If there is a doc, remove the id's that are in the reviews array.
+  if (doc) {
+    await review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema)  // export created model
