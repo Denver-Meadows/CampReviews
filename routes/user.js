ec9bfcb29b98bc4800b2495b5e6ad76bba8a496a
Denver-Meadows <meadows8200@gmail.com>
2021-09-07 20:12:12 -0400

    Basic user registration setup

--- routes/user.js
+++ routes/user.js
@@ -11,13 +11,19 @@ router.get('/', (req, res) => {
 })
 
 // Post route to post data from form
+// Adding a try/catch in addition to the catchAsync
 router.post('/', catchAsync(async (req, res) => {
-  const {email, username, password} = req.body;
-  const user = new User({email, username})   // create new user instance but do not pass in the password.
-  const registeredUser = await User.register(user, password); // The register method will register a new user with a given pw.  Checks if username is unique. It will handle all of the hashing and salt with the pw it stores. 
-  console.log(registeredUser)
-  req.flash('success', 'Welcome to YelpCamp')
-  res.redirect('/campgrounds')
+  try {
+      const {email, username, password} = req.body;
+      const user = new User({email, username})   // create new user instance but do not pass in the password.
+      const registeredUser = await User.register(user, password); // The register method will register a new user with a given pw.  Checks if username is unique. It will handle all of the hashing and salt with the pw it stores. 
+      console.log(registeredUser)
+      req.flash('success', 'Welcome to YelpCamp')
+      res.redirect('/campgrounds')
+  } catch(e) {
+      req.flash('error', e.message)
+      res.redirect('register')
+  }
 }))
 
 
