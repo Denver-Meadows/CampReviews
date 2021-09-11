5a46275461fd70862430970f82ef529550a199c5
Denver-Meadows <meadows8200@gmail.com>
2021-09-11 13:55:09 -0400

    refactored routes with router.route

--- routes/campground.js
+++ routes/campground.js
@@ -4,25 +4,20 @@ const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware/middleware');
 const campgrounds = require('../controllers/campgrounds');
 
-// Index
-router.get('/', catchAsync(campgrounds.index))
+// Usering router.route to chain commands on a specific route (refactoring all routes)
+router.route('/')
+  .get(catchAsync(campgrounds.index)) // index
+  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)) // 2nd Part of Create (posting data from form)
 
 // Create
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-// 2nd Part of Create (posting data from form)
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
-// Show
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+  .get(catchAsync(campgrounds.showCampground)) // Show
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground)) // Delete
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground)) // Put for 2nd part of edit -- isLoggenIn gives us access to req.user
 
 // Edit
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-// Delete
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
-
-// Put for 2nd part of edit -- isLoggenIn gives us access to req.user
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,17 @@ const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const ExpressError = require('../utilities/ExpressError');
 const passport = require('passport');
-const users = require('../controllers/users')
+const users = require('../controllers/users');
+const { route } = require('./campground');
 
-// Route to create new user (render form)
-router.get('/register', users.renderRegisterForm)
+// Chaining routes with router.route
+router.route('/register')
+  .get(users.renderRegisterForm) // Route to create new user (render form)
+  .post(catchAsync(users.registerNewUser)) // Post route to post data from form
 
-// Post route to post data from form
-router.post('/register', catchAsync(users.registerNewUser));
-
-// routes for logging in
-router.get('/login', users.renderLoginForm);
-
-// Post route for login.  
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.loginUser)
+router.route('/login')
+  .get(users.renderLoginForm) // routes for logging in
+  .post(passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.loginUser) // Post route for login.  
 
 // route to logout.  Passport gives us access to a logout method on the req object, which logs out the user.
 router.get('/logout', users.logoutUser)
