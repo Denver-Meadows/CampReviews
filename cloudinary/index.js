88ff64081ba8680ea3dcbce96c03337c22626d6e
Denver-Meadows <meadows8200@gmail.com>
2021-09-14 18:44:11 -0400

    BUG FINALLY FIXED

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,23 +1,22 @@
+
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// Set the config on cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
 });
 
-// Setting up an instance
 const storage = new CloudinaryStorage({
-  cloudinary, // passing in above cloudinary.config
-  params: {
-    folder: 'CampReviews', // folder in cloudinary to store this data
-    allowedFormats: ['jpeg', 'png', 'jpg']
-  }
+    cloudinary,
+    params: {
+        folder: 'CampReviews',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
- 
+
 module.exports = {
-  cloudinary, 
-  storage
+    cloudinary,
+    storage
 }
--- routes/campground.js
+++ routes/campground.js
@@ -2,23 +2,27 @@
 if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 };
+require("dotenv").config();
 
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware/middleware');
 const campgrounds = require('../controllers/campgrounds');
-const multer = require('multer');
+const multer = require('multer'); // adds a req.body which contains values and req.file which contains files.
 const { storage } = require('../cloudinary');
-// const upload = multer({ dest: 'uploads/' })
 const upload = multer({ storage }); // tell multer where to store the files which is the new storage we created with multer/cloudinary
-
 
 // Usering router.route to chain commands on a specific route (refactoring all routes)
 router.route('/')
   .get(catchAsync(campgrounds.index)) // index
-  // .post((req, res) => res.send(req.body))
-  .post(isLoggedIn, validateCampground ,catchAsync(campgrounds.createCampground)) // 2nd Part of Create (posting data from form)
+  .post(upload.single('images'), (req, res) => {
+    res.send('working')
+    console.log(req.file)
+    console.log(req.body)
+  })
+  
+  // .post(isLoggedIn, validateCampground ,catchAsync(campgrounds.createCampground)) // 2nd Part of Create (posting data from form)
 
 // Create upload.array('image'),
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
