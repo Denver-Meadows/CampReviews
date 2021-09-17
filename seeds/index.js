3394545b8ea784e46d3b7b64c52c2b97df9aa9cf
Denver-Meadows <meadows8200@gmail.com>
2021-09-17 18:28:55 -0400

    DB reseeded with proper coordinates

--- seeds/index.js
+++ seeds/index.js
@@ -35,7 +35,10 @@ const seedDB = async () => {
       price: price,
       geometry: {
          type: "Point", 
-         coordinates: [-113.1331, 47.0202] 
+         coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ] 
       },
       images: [
         {
