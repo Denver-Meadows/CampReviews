9ebf07e6de93dce1c315a5a8e85aa9a64bbb225e
Denver-Meadows <meadows8200@gmail.com>
2021-09-18 19:43:17 -0400

    Implementing validate function for Joi to sanitize HTML

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 // If not in production -- get access to the .env variables
 if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
-};
+  require("dotenv").config();
+}
 //console.log(process.env.CLOUDINARY_SECRET) // Seeing how to get this info
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3000;
 
-const User = require('./models/user') // importing User Model
+const User = require("./models/user"); // importing User Model
 
-const campgroundRoutes = require('./routes/campground'); // Require routes (will need to add the app.use below to init)
-const reviewRoutes = require('./routes/reviews');
-const userRoutes = require('./routes/user')
+const campgroundRoutes = require("./routes/campground"); // Require routes (will need to add the app.use below to init)
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/user");
 
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const passportStrategy = require('passport-local');
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const passportStrategy = require("passport-local");
+
+const mongoSanitize = require("express-mongo-sanitize");
 
 // Importing catchAsync
-const catchAsync = require('./utilities/catchAsync');
-const ExpressError = require('./utilities/ExpressError');
+const catchAsync = require("./utilities/catchAsync");
+const ExpressError = require("./utilities/ExpressError");
 
 // ejs-mate is an add-on for ejs that helps make designing views easy.  We can create a boilerplate that is shared across all pages.
-const ejsMate = require('ejs-mate')
-const methodOverride = require('method-override');
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/camp-review', {
-  useNewUrlParser: true, 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/camp-review", {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
 // Mongoose Connection error handling
-mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error:")
+);
 mongoose.connection.once("open", () => {
   console.log("Database connected");
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Configuring session
 const sessionConfig = {
-  secret: 'willNeedToUpdateThisSecretForProduction',
+  secret: "willNeedToUpdateThisSecretForProduction",
   resave: false,
   saveUninitialized: true,
   // We can add some additional options for the cookie
@@ -55,18 +60,19 @@ const sessionConfig = {
     httpOnly: true, // helps with security
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // expires in a week.  Date.now is in milliseconds so we need the match to convert
     maxAge: 1000 * 60 * 60 * 24 * 7, // maxAge is a week
-  }
+  },
 };
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
 
-app.engine('ejs', ejsMate); // Tell the app we are using ejsMate as the engine that runs, parse's and basically makes sense of EJS instead of the default. With this we can define a layout file.
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method')) // pass in query string we want to use
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine("ejs", ejsMate); // Tell the app we are using ejsMate as the engine that runs, parse's and basically makes sense of EJS instead of the default. With this we can define a layout file.
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // pass in query string we want to use
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize()); // security for queries
 
 // PASSPORT
-app.use(passport.initialize()); 
+app.use(passport.initialize());
 app.use(passport.session()); // Needed if we want persisent login with passport.  Must be used AFTER session
 // Now we must tell passport to use the passportStrategy we required and for that passportStrategy, the authentication method will be on the User model.
 // authenticate is a method from created by passport.
@@ -79,34 +85,33 @@ passport.deserializeUser(User.deserializeUser()); // opposite of above.  How to 
 
 // middleware for flash -- this makes these available on all templates
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user // added later with passport and makes this available on all ejs files
-  res.locals.success = req.flash('success'); // with this, we'll have access to 'success' variable on all requests.
-  res.locals.error = req.flash('error');
+  res.locals.currentUser = req.user; // added later with passport and makes this available on all ejs files
+  res.locals.success = req.flash("success"); // with this, we'll have access to 'success' variable on all requests.
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
 // Init routes
-app.use('/', userRoutes)
-app.use('/campgrounds', campgroundRoutes)
-app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-
-app.get('/', (req, res) => {
-  res.render('home')
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 // Using all for all types of requests and * for all paths, if not found send 404 alert
-app.all('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
 // basic error handling
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'Oh No, Something Went Wrong!';
-  res.status(statusCode).render('error', { err }); // passing in the entire error
-})
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err }); // passing in the entire error
+});
 
 app.listen(port, () => {
-  console.log('Listening on 3000')
-})
+  console.log("Listening on 3000");
+});
--- schemas.js
+++ schemas.js
@@ -1,6 +1,30 @@
 // List of schemas that we want Joi to validate
 
-const Joi = require('joi');
+const Joi = require("joi");
+
+// Making an extension to safeguard aganist XSS
+// We are making a function called validate, which Joi will checked based on whatever value it receives.
+// Also using sanitize-html
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapedHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
 
 module.exports.campgroundSchema = Joi.object({
   title: Joi.string().required(),
@@ -8,7 +32,7 @@ module.exports.campgroundSchema = Joi.object({
   location: Joi.string().required(),
   // image: Joi.string().required(),
   description: Joi.string().required(),
-  deleteImages: Joi.array()
+  deleteImages: Joi.array(),
 });
 
 module.exports.reviewSchema = Joi.object({
