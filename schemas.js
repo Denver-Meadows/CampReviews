b437ebdc36cce74b8d6e354f7321b6f47e108737
Denver-Meadows <meadows8200@gmail.com>
2021-09-18 19:54:19 -0400

    sanitizing html added

--- schemas.js
+++ schemas.js
@@ -1,6 +1,7 @@
 // List of schemas that we want Joi to validate
 
-const Joi = require("joi");
+const baseJoi = require("joi"); // Standard Joi -- Below we will pass in an extension for baseJoi.
+const sanitizeHtml = require("sanitize-html");
 
 // Making an extension to safeguard aganist XSS
 // We are making a function called validate, which Joi will checked based on whatever value it receives.
@@ -19,23 +20,25 @@ const extension = (joi) => ({
           allowedAttributes: {},
         });
         if (clean !== value)
-          return helpers.error("string.escapedHTML", { value });
+          return helpers.error("string.escapeHTML", { value });
         return clean;
       },
     },
   },
 });
 
+const Joi = baseJoi.extend(extension); // The extension was added later so we are naming it Joi so we don't have to update the below code.
+
 module.exports.campgroundSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().required().escapeHTML(),
   price: Joi.number().required().min(0),
-  location: Joi.string().required(),
+  location: Joi.string().required().escapeHTML(),
   // image: Joi.string().required(),
-  description: Joi.string().required(),
+  description: Joi.string().required().escapeHTML(),
   deleteImages: Joi.array(),
 });
 
 module.exports.reviewSchema = Joi.object({
   rating: Joi.number().required(),
-  body: Joi.string().required(),
+  body: Joi.string().required().escapeHTML(),
 });
