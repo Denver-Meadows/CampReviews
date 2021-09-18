c5bc1b7914a33def6e5f840eb05699e6fdcc8df9
Denver-Meadows <meadows8200@gmail.com>
2021-09-18 18:22:10 -0400

    map controls added

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,17 +2,18 @@
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
-  zoom: 7 // starting zoom
+  zoom: 7, // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
-    new mapboxgl.Popup({offset: 25})
-    .setHTML(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
       `<h4>${campground.title}</h4><p>${campground.location}</p>`
     )
   )
