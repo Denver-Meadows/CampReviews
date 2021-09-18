e58780f17d841d3ea39f079cfad29e769d5ed0b8
Denver-Meadows <meadows8200@gmail.com>
2021-09-18 15:03:29 -0400

    Custom cluster map working with custom popups

--- models/campground.js
+++ models/campground.js
@@ -1,66 +1,79 @@
-const mongoose = require('mongoose');
-const review = require('./review');
-const Schema = mongoose.Schema;  // shortcut for mongoose.Schema
+const mongoose = require("mongoose");
+const review = require("./review");
+const Schema = mongoose.Schema; // shortcut for mongoose.Schema
 
-const CampgroundSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  images: [
-    {
-      url: String,
-      filename: String,
-    }
-  ],
-  geometry: {
-    type: {
+// **Mongoose does not include virtuals when you convert a doc to JSON.  Therefore it won't show on the model's object.
+// So we need to set the below options and pass that in the below Campground Schema (at the end.)
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
+    title: {
       type: String,
-      enum: ['Point'],
       required: true,
     },
-    coordinates: {
-      type: [Number],
+    images: [
+      {
+        url: String,
+        filename: String,
+      },
+    ],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    price: {
+      type: Number,
       required: true,
-    }
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    author: {
+      // will be a ref so we need ObjectId so we can reference the other Model in the db. (populate will show this info)
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // this is an ObjectID
+        ref: "Review", // The reference model
+      },
+    ],
   },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  author: {
-    // will be a ref so we need ObjectId so we can reference the other Model in the db. (populate will show this info)
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  reviews: [
-    {
-      type: mongoose.Schema.Types.ObjectId, // this is an ObjectID
-      ref: "Review", // The reference model
-    }
-  ],
+  opts // passing in the options created above
+);
+
+// Registering a virtual property
+// Using a virtual that will nest properties.popUpMarkup.
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<a href="/campgrounds/${this._id}">${this.title}`;
 });
 
 // Mongoose middleware for deleting.  Post will run after 'findOneAndDelete' runs. Which is being ran when we delete a campground with 'findByIdAndDelete'.  Check the mongoose docs but this is their middleware that runs.
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
   // console.log(doc) // the doc is the data that was deleted.
   // If there is a doc, remove the id's that are in the reviews array.
   if (doc) {
     await review.deleteMany({
       _id: {
-        $in: doc.reviews
-      }
-    })
+        $in: doc.reviews,
+      },
+    });
   }
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema)  // export created model
+module.exports = mongoose.model("Campground", CampgroundSchema); // export created model
--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -93,9 +93,8 @@ map.on("load", () => {
   // the location of the feature, with
   // description HTML from its properties.
   map.on("click", "unclustered-point", (e) => {
+    const { popUpMarkup } = e.features[0].properties; // saving our new property from the virtual in the model
     const coordinates = e.features[0].geometry.coordinates.slice();
-    const mag = e.features[0].properties.mag;
-    const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -104,14 +103,11 @@ map.on("load", () => {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
-      .addTo(map);
+    // passing in the new 'text' above
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
   });
 
   map.on("mouseenter", "clusters", () => {
-    console.log("mouse over cluster");
     map.getCanvas().style.cursor = "pointer";
   });
   map.on("mouseleave", "clusters", () => {
