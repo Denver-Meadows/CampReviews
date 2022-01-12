d6a2442f1dada283e523e4b8d9e07697f634896c
Denver-Meadows <meadows8200@gmail.com>
2022-01-12 15:45:04 -0500

    heroku settings updated for deployment

--- app.js
+++ app.js
@@ -121,8 +121,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err }); // passing in the entire error
 });
 
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
